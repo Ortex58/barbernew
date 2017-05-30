f3f32c38adc6e7fed0c1e0f7cd9def8b633acba0
Ortex <ortex@i.ua>
2017-05-30 10:59:10 +0000

    login focus

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var hide = document.querySelector('.login');
 var form = document.querySelector('#form');
-var show = document.querySelector('.login');
+var autofocus = form.querySelector('[name=login]');
 function login() {
 	hide.addEventListener('click', function(e){
 		e.preventDefault();
 		if (form.classList.contains('hiden')) {
 			form.classList.remove('hiden');
+			autofocus.focus();
 		}
 		else {
 			form.classList.add('hiden');
