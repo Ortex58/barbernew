e3def97324cd39d80a47b574c5257c62464db8a8
Ortex <ortex@i.ua>
2017-06-07 08:15:26 +0000

    finisg 1th page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var hide = document.querySelector('.login');
+/*var hide = document.querySelector('.login');
 var form = document.querySelector('#form');
 var autofocus = form.querySelector('[name=login]');
 function login() {
@@ -7,10 +7,38 @@ function login() {
 		if (form.classList.contains('hiden')) {
 			form.classList.remove('hiden');
 			autofocus.focus();
+			//document.body.style.overflow = 'hidden';
 		}
 		else {
 			form.classList.add('hiden');
+			//document.body.style.overflow = 'visible';
 		}
 	})
 }
-login();
+
+	window.onclick = function (event) {
+		if (event.target == form) {
+		form.classList.add('hiden');
+		}
+	}
+login();*/
+var modal = document.querySelector('#myModal');
+var btn = document.querySelector('.login');
+var span = document.querySelector('.close');
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+span.onclick = function() {
+  modal.style.display = 'none';
+  document.body.style.overflow = 'visible';
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'visible';
+  }
+}
